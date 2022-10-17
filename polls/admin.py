fb523660800d38dcedd677145cbec9cb634fc554
Brittany Yarbrough <brittanymyar@gmail.com>
2022-10-16 23:52:01 -0400

    initial commit

---
+++ polls/admin.py
@@ -0,0 +1,44 @@
+from .models import Choice, Question
+from django.contrib import admin
+
+from .models import Question
+
+admin.site.register(Question)
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+
+admin.site.register(Question, QuestionAdmin)
+
+admin.site.register(Choice)
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': [
+         'pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
